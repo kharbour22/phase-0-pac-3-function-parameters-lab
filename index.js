acb1b44fe2966f481c4fde1e34cdc10eecea0a9e
KevinHarbour <kharbour22@gmail.com>
2024-01-08 11:15:32 -0700

    done

---
+++ index.js
@@ -0,0 +1,17 @@
+
+
+function introduction(name) {
+    if (name === 'Aki')
+    return"Hi, my name is Aki."
+if (name === 'Samip')
+return"Hi, my name is Samip."
+}
+
+function introductionWithLanguage(name, language) {
+    return "Hi, my name is " + name + " and I am learning to program in " + language + ".";
+}
+
+function introductionWithLanguageOptional(name, language = "JavaScript") {
+    return "Hi, my name is " + name + " and I am learning to program in " + language + ".";
+}
+   
